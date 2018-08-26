6e8411c5911ab31cc6bcce4287f144fdfc97fc0f
tranhuyhoangbka <hoangth92.nd@gmail.com>
2018-08-26 12:30:07 +0700

    rspec gem and auto load path config

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module FirstDemo
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
